e86052aa2dc53678f0f010a80513d62403ccabc5
Jason Marvin <jdmarvin2002@yahoo.com>
2017-02-27 11:35:52 -0700

    Added Coins
    Added coins to the scene, added the “poof” animation/particles,
    destroyed the coins and added a sound.

--- vrnd-course3-starter-project 2/Assets/Udacity/Scripts/Coin.cs
+++ vrnd-course3-starter-project 2/Assets/Udacity/Scripts/Coin.cs
@@ -5,11 +5,21 @@ using UnityEngine;
 public class Coin : MonoBehaviour 
 {
     //Create a reference to the CoinPoofPrefab
+	public GameObject poofAnimation;
 
     public void OnCoinClicked() {
         // Instantiate the CoinPoof Prefab where this coin is located
+		Object.Instantiate(poofAnimation, transform.position, Quaternion.AngleAxis(90, Vector3.left));
         // Make sure the poof animates vertically
         // Destroy this coin. Check the Unity documentation on how to use Destroy
+		Destroy (gameObject);
     }
 
+	void Update() {
+	
+		transform.Rotate(Vector3.right * Time.deltaTime);
+	}
+
+
 }
+//Camera.main.transform.position = gameObject.transform.position;
