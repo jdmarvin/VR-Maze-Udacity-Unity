fd730291373ba23fd737225da0d528b7083de7fa
Jason Marvin <jdmarvin2002@yahoo.com>
2017-02-27 11:49:58 -0700

    Created CoinPoof
    Created a duplicate of the Poof animation, called it CoinPoof and
    referenced it to the coins.

--- vrnd-course3-starter-project 2/Assets/Udacity/Scripts/Coin.cs
+++ vrnd-course3-starter-project 2/Assets/Udacity/Scripts/Coin.cs
@@ -9,16 +9,12 @@ public class Coin : MonoBehaviour
 
     public void OnCoinClicked() {
         // Instantiate the CoinPoof Prefab where this coin is located
+		// Make sure the poof animates vertically
 		Object.Instantiate(poofAnimation, transform.position, Quaternion.AngleAxis(90, Vector3.left));
-        // Make sure the poof animates vertically
+
         // Destroy this coin. Check the Unity documentation on how to use Destroy
 		Destroy (gameObject);
     }
-
-	void Update() {
-	
-		transform.Rotate(Vector3.right * Time.deltaTime);
-	}
 
 
 }
