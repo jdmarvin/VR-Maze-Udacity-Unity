3359ee46082b2a6a2fefa1653c3a0ebfcab10db9
Jason Marvin <jdmarvin2002@yahoo.com>
2017-03-01 10:39:13 -0700

    Coin Float
    Created an animation to make the coin float.

--- vrnd-course3-starter-project 2/Assets/Udacity/Scripts/Coin.cs
+++ vrnd-course3-starter-project 2/Assets/Udacity/Scripts/Coin.cs
@@ -7,6 +7,12 @@ public class Coin : MonoBehaviour
     //Create a reference to the CoinPoofPrefab
 	public GameObject coinPoof;
 
+
+	void Update() {
+
+		transform.Translate (0, 0.05f * Mathf.Sin(Time.time * 5.0f), 0, Space.World);
+	}
+
     public void OnCoinClicked() {
         // Instantiate the CoinPoof Prefab where this coin is located
 		// Make sure the poof animates vertically
